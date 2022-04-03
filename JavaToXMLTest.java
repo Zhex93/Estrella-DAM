1dc653eba2d478454a4411656f7e4f629ac8db9a
Zhex93 <dani_mbd@hotmail.com>
2022-04-03 19:02:37 +0200

    Commit inicial
    Primer commit de prueba.

---
+++ JavaToXMLTest.java
@@ -0,0 +1,9 @@
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
+
+public class JavaToXMLTest {
+    public static void main(String[] args) {
+        
+    }
+}
